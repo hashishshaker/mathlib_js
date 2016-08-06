4f1246c0716ce0ddefdd83863109fcdabdb6243f
Shashi Shekhar <hashishshaker@users.noreply.github.com>
2016-08-06 23:45:04 +0530

    Add files via upload

--- NumberHelper.js
+++ NumberHelper.js
@@ -26,6 +26,7 @@ function NumberHelper() {
   * Generates a sequence of Fibonacci Series that includes a0 and a1, which are passed as parameters
   * @param {Number} a0 - Positive Integer that signifies the first number in the sequence
   * @param {Number} a1 - Positive Integer that signifies the second number in the sequence
+  * @param {Number} numberRequired - Positive Integer that denotes the number of terms of the Fibonacci series to be generated
   * @return {JSON} A JSON array that contains the list of the Fibonacci Series, invalid input error if a0 and a1 are not positive integers, Error is a0 >= a1, Error if a0 or a1 is larger than the largest number supported by Javascript
   */
   function getFibonacciSeries(a0, a1, numberRequired) {
@@ -51,9 +52,36 @@ function NumberHelper() {
     }
   }
 
+  /*
+  * Generates a sequence of Fibonacci Series that includes a0 and a1, which are passed as parameters upto an upper limit that is specified as one of the parameters
+  * @param {Number} a0 - Positive Integer that signifies the first number in the sequence
+  * @param {Number} a1 - Positive Integer that signifies the second number in the sequence
+  * @param {Number} upperLimit - Positive Integer that denotes the number up till the terms of the Fibonacci series are to be generated
+  * @return {JSON} A JSON array that contains the list of the Fibonacci Series, invalid input error if a0 and a1 are not positive integers, Error is a0 >= a1, Error if a0 or a1 or upperLimit is larger than the largest number supported by Javascript
+  */
+  function getFibonacciSeriesUpto(a0, a1, upperLimit) {
+    if (a0 >= 0 && a1 >= 1 && a1 > a0 && Number.isInteger(a0) && Number.isInteger(a1) && a0 < Number.MAX_SAFE_INTEGER && a1 < Number.MAX_SAFE_INTEGER && upperLimit <= Number.MAX_SAFE_INTEGER) {
+      var result = [];
+      var temp;
+      result.push(a0);
+      result.push(a1);
+      var a_n_1 = a0;
+      var a_n = a1;
+      while ((temp = a_n_1 + a_n) <= upperLimit) {
+        result.push(temp);
+        a_n_1 = a_n;
+        a_n = temp;
+      }
+      return result;
+    } else {
+      return Error('invalid input');
+    }
+  }
+
   return {
     checkPrime: checkPrime,
-    getFibonacciSeries: getFibonacciSeries
+    getFibonacciSeries: getFibonacciSeries,
+    getFibonacciSeriesUpto: getFibonacciSeriesUpto
   }
 };
 
--- README.md
+++ README.md
@@ -19,6 +19,7 @@ console.log(nh().checkPrime(2554546454568632135454698765463132135454686654545463
 ```
 
 ## getFibonacciSeries
+
 Generates a sequence of Fibonacci Series that includes a0 and a1, which are passed as parameters
 @param {Number} a0 - Positive Integer that signifies the first number in the sequence
 @param {Number} a1 - Positive Integer that signifies the second number in the sequence
@@ -28,3 +29,10 @@ Generates a sequence of Fibonacci Series that includes a0 and a1, which are pass
 var nh = require('math-lib');
 console.log(nh().getFibonacciSeries(0, 1, 10)); // returns [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
 ```
+## getFibonacciSeriesUpto
+
+Generates a sequence of Fibonacci Series that includes a0 and a1, which are passed as parameters upto an upper limit that is specified as one of the parameters
+@param {Number} a0 - Positive Integer that signifies the first number in the sequence
+@param {Number} a1 - Positive Integer that signifies the second number in the sequence
+@param {Number} upperLimit - Positive Integer that denotes the number up till the terms of the Fibonacci series are to be generated
+@return {JSON} A JSON array that contains the list of the Fibonacci Series, invalid input error if a0 and a1 are not positive integers, Error is a0 >= a1, Error if a0 or a1 or upperLimit is larger than the largest number supported by Javascript
